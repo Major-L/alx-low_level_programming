779d86bcbd37929d89f3f2f988a00bb0322de898
Major-L <sirlmoruri@gmail.com>
2022-09-09 10:52:12 -0700

    Positive anything is better than negative nothing

---
+++ 1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print random number and say positive, negative or zero
+ * Return:0
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+		printf("%i is negative\n", n);
+	if (n > 0)
+		printf("%i is positive\n", n);
+	if (n == 0)
+		printf("%i is zero\n", n);
+	return (0);
+}
